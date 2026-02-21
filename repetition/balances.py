14ac9afc3b126a84eac4bfb9fe37514dd08797e6
art7235 <Art7235B@gmail.com>
2026-02-21 12:19:31 +0300

    добавил 2 задачи

---
+++ repetition/balances.py
@@ -0,0 +1,16 @@
+balans = 1000
+
+string = int(input("выберите действие: "))
+
+while string != 0:
+	if string == 1:
+		print(f"на вашес счету: {balans}")
+	if string == 2:
+		balans += int(input("Введите сумму:"))
+	if string == 3:
+		a = int(input("Введите сумму:"))
+		if balans - a >= 0:
+			balans -= a
+		else:
+			print("Недостаточно средств")
+	string =  int(input("выберите действие: "))
