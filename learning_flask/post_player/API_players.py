a01add0e87dacee5ab2305c7fa717db277da8629
art7235 <Art7235B@gmail.com>
2026-08-01 23:23:25 +0300

    Добавил золото игроков, эндпоинты пополнения баланса, лидерборда и статистики

--- learning_flask/post_player/API_players.py
+++ learning_flask/post_player/API_players.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 class Player(BaseModel):
     name: str
     level: int
+    gold: int
     created_at: datetime
   
 
@@ -24,10 +25,29 @@ def create_player():
 
         if name is not None and level is not None:
             player_id += 1
-            player = Player(name=name, level=level, created_at=datetime.now())
+            player = Player(name=name, level=level, gold=0, created_at=datetime.now())
             players[str(player_id)] = player
             return {"message": "Player created",
                     "player": player.model_dump_json()}, 200
+
+
+@app.route('/player/gold', methods=['POST'])
+def top_up_balance():
+    data = request.get_json(silent=True)
+    if isinstance(data, dict):
+        name = data.get('name')
+        gold = data.get('gold')
+
+        if name is not None and gold is not None:
+            for key, i in players.items():
+                if name == i.name:
+                    i.gold += gold
+                    return {"message": "balance replenished",
+                            "player": i.model_dump()}, 200
+            return {"error": "User not found"}, 404
+    
+    return {"error": "Invalid request"}, 400
+
 
 
 @app.route('/players', methods=['GET'])
@@ -69,6 +89,32 @@ def delete_player():
             break
         
     return {"message" : "player was deleted"}, 200
+
+
+@app.route('/leaderboard', methods=['GET'])
+def ret_leaderboard():
+    players_list = list(players.items())
+    players_list.sort(key=lambda item: item[1].level, reverse=True)
+    res = []
+    for player_id, player in players_list:
+        res.append({
+            "name": player.name,
+            "level": player.level
+        })
+    return {"players": res}, 200
+
+
+@app.route('/stats', methods=['GET'])
+def ret_stats():
+    max_level = 0
+    averge_level = 0
+    for key, i in players.items():
+        averge_level+=i.level
+        if i.level > max_level:
+            max_level = i.level
+    return {"players_count": len(players),
+    "max_level": max_level,
+    "average_level": averge_level/len(players)}, 200
 
 
 if __name__ == "__main__":
